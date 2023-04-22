0cb3ac45f7bf22bb8ce96cec979f2959c026ca65
David Cassiet <davidcassiet@gmail.com>
2023-04-22 20:11:49 -0300

    ...

--- src/main/java/org/example/repository/ClientRepository.java
+++ src/main/java/org/example/repository/ClientRepository.java
@@ -6,13 +6,4 @@ import java.util.ArrayList;
 
 public interface ClientRepository {
     ArrayList<Client> clients = new ArrayList<>();
-
-    default Client getClientById(String clientId) {
-        for (Client actualClient : clients) {
-            if (actualClient.getId().equals(clientId)) {
-                return actualClient;
-            }
-        }
-        return null;
-    }
 }
